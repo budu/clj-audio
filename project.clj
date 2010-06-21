9e6eed379bca342c5ed9629739edf8eb2da066fb
Nicolas Buduroi <nbuduroi@gmail.com>
2010-06-21 16:53:18 -0400

    Added a description of the project.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject clj-audio "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+
+(defproject clj-audio "0.1.0-SNAPSHOT"
+  :description "Idiomatic Clojure wrapper for the Java Sound API."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]])
