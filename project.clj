34cb410f129b8b8b66aaf47c760385097ff22097
Nicolas Buduroi <nbuduroi@gmail.com>
2010-06-21 16:57:47 -0400

    Added development dependencies for swank and clojars.

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
 (defproject clj-audio "0.1.0-SNAPSHOT"
   :description "Idiomatic Clojure wrapper for the Java Sound API."
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]])
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+                     [swank-clojure "1.2.1"]])
