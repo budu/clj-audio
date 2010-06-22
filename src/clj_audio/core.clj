ea1a20a5323229816e210431b3d716f1f4ae5ba3
Nicolas Buduroi <nbuduroi@gmail.com>
2010-06-21 21:14:26 -0400

    Added the play function and code to choose a mixer.

--- src/clj_audio/core.clj
+++ src/clj_audio/core.clj
@@ -9,9 +9,12 @@
 (ns #^{:author "Nicolas Buduroi"
        :doc "Clojure support for audio."}
   clj-audio.core
+  (:use clj-audio.sampled
+        [clojure.contrib.def :only [defvar]])
   (:import [javax.sound.sampled
             AudioInputStream
             AudioSystem
+            SourceDataLine
             TargetDataLine]))
 
 ;;;; Audio input streams
@@ -35,3 +38,60 @@
 (defmethod ->stream java.io.InputStream
   [stream fmt length]
   (AudioInputStream. stream fmt length))
+
+;;;; Mixer
+
+(defn mixers
+  "Returns a list of all available mixers."
+  []
+  (map #(AudioSystem/getMixer %)
+       (AudioSystem/getMixerInfo)))
+
+(defmacro with-mixer
+  "Creates a binding to the given mixer then executes body. The mixer
+  will be bound to *mixer* for use by other functions."
+  [mixer & body]
+  `(binding [*mixer* ~mixer]
+     ~@body))
+
+;;;; Playback
+
+(def default-buffer-size (* 64 1024))
+
+(defvar *line-buffer-size*
+  default-buffer-size
+  "Line buffer size in bytes, must correspond to an integral number of
+  frames.")
+
+(defvar *playback-buffer-size*
+  default-buffer-size
+  "Playback buffer size in bytes.")
+
+(defvar *playing*
+  (atom false)
+  "Variable telling if play* is currectly writing to a line. If set to
+  false during playback, play* will exit.")
+
+(defn play*
+  "Write the given audio stream bytes to the given source data line
+  using a buffer of the given size. Returns the number of bytes played."
+  [#^SourceDataLine source audio-stream buffer-size]
+  (let [buffer (byte-array buffer-size)]
+    (compare-and-set! *playing* false true)
+    (loop [cnt 0 total 0]
+      (if (and (> cnt -1) @*playing*)
+        (do
+          (when (> cnt 0)
+            (.write source buffer 0 cnt))
+          (recur (.read audio-stream buffer 0 (alength buffer))
+                 (+ total cnt)))
+        total))
+    (compare-and-set! *playing* true false)))
+
+(defn play
+  "Play the given audio stream."
+  [audio-stream]
+  (with-data-line [source (make-line :output
+                                     (->format audio-stream)
+                                     *line-buffer-size*)]
+    (play* source audio-stream *playback-buffer-size*)))
