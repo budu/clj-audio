fd844d89241575be5bf7fa2f022caa54d96540c5
Nicolas Buduroi <nbuduroi@gmail.com>
2010-06-22 19:33:33 -0400

    Added clj-help has development dependencies, can't use the REPL without it anymore.

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [swank-clojure "1.2.1"]])
+                     [swank-clojure "1.2.1"]
+                     [clj-help "0.1.0-SNAPSHOT"]])
