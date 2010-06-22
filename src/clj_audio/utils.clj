159accd898506b13c0e556cc2af4fafb516f8be6
Nicolas Buduroi <nbuduroi@gmail.com>
2010-06-22 19:36:51 -0400

    Changed wrap-enum to be able to return a map with constants as keys.

--- src/clj_audio/utils.clj
+++ src/clj_audio/utils.clj
@@ -14,19 +14,25 @@
 (defn clojurize-constant-name [name]
   (.replace (.toLowerCase (str name)) "_" "-"))
 
-(defn enum->map [names constants]
-  (into {} (map #(vector (keyword (clojurize-constant-name %1)) %2)
-                names
-                constants)))
-
 (defn get-static-fields [klass]
   (filter #(Modifier/isStatic (.getModifiers %))
           (.getFields klass)))
 
-(defn wrap-enum [klass]
+(defn enum->map [names constants & [constants-as-keys]]
+  (into {} (map #(let [n (keyword (clojurize-constant-name %1))
+                       c %2]
+                   (if constants-as-keys
+                     (vector c n)
+                     (vector n c)))
+                names
+                constants)))
+
+(defn wrap-enum [klass & [constants-as-keys]]
   (if (isa? klass Enum)
     (let [cs (.getEnumConstants klass)]
-      (enum->map (map str cs) cs))
+      (enum->map (map str cs) cs
+                 constants-as-keys))
     (let [cs (get-static-fields klass)]
       (enum->map (map #(.getName %) cs)
-                 (map #(.get % nil) cs)))))
+                 (map #(.get % nil) cs)
+                 constants-as-keys))))
