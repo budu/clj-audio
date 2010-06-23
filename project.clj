ca5490956da4b3b10ec79bff7f8bde61e2a06c3f
Nicolas Buduroi <nbuduroi@gmail.com>
2010-06-23 19:20:34 -0400

    Slight dependency change.

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
                  [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [swank-clojure "1.2.1"]
-                     [clj-help "0.1.0-SNAPSHOT"]])
+                     [clj-help "0.1.1"]])
