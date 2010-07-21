3e4c2a337dda5092c7d0bc3d8b2c0533ff71d4bb
Nicolas Buduroi <nbuduroi@gmail.com>
2010-07-21 14:29:20 -0400

    Started working on 0.2.0 release.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
-(defproject clj-audio/clj-audio "0.1.0"
+(defproject clj-audio/clj-audio "0.2.0-SNAPSHOT"
   :description "Idiomatic Clojure wrapper for the Java Sound API."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
