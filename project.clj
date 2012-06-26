55196f1fffe3d5153f74730e55ff66a70f6fd85e
Wade Mealing <wmealing@gmail.com>
2012-06-27 00:44:39 +1000

    Remove old clojure and clojure.contrib which are no longer used.

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 
 (defproject clj-audio/clj-audio "0.2.0-SNAPSHOT"
   :description "Idiomatic Clojure wrapper for the Java Sound API."
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [swank-clojure "1.2.1"]
                      [clj-help "0.1.1"]])
